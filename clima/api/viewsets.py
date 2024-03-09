dfd36fe7cbae777ae8bce13dce83b75b8ad86e2b
Caio Soares <99054227+1caio1@users.noreply.github.com>
2024-03-09 20:57:43 -0300

    Update viewsets.py

--- clima/api/viewsets.py
+++ clima/api/viewsets.py
@@ -10,26 +10,32 @@ class TempoViewset(viewsets.ModelViewSet):
     serializer_class = PrevisaodoTempoSerializer
     queryset = PrevisaodoTempo.objects.all()
 
+#funcao create 
     def create(self, request):
         cidade = request.data.get('cidade', '')
-        chave_da_api = '8fb56de87ff0a40e6b526e4ed0c15dcb'  # chave da api
+        chave_da_api = '8fb56de87ff0a40e6b526e4ed0c15dcb' # chave da api
         # url
         url = f'http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave_da_api}&units=metric'
       
-        response = requests.get(url)
-        dados = response.json()
+        respostas = requests.get(url)
+        dados = respostas.json()
+        #dados captados para api
         name = dados.get('name', '')
         temperatura = dados.get('temp', '')
         temperaturaMaxima = dados.get('temp_max',  '')
         temperaturaMinima = dados.get('temp_min', '')
+        
+        #referenciando json
         recebido = {
-            'cidade': name,
-            'temperatura': temperatura,
-            'temperatura_maxima': temperaturaMaxima,
-            'temperatura_minima': temperaturaMinima
+            'cidade': f'{name}',
+            'temperatura': f'{temperatura}',
+            'temperatura_maxima': f'{temperaturaMaxima}',
+            'temperatura_minima': f'{temperaturaMinima}',
         }
+        
         serialize = PrevisaodoTempoSerializer(data=recebido)
         if serialize.is_valid():
+            serialize.save()
            
             return Response(serialize.data, status=status.HTTP_201_CREATED)
         else:
